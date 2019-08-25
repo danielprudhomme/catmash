788369515ef02c366c05730907ad27cea7bbe826
danielprudhomme <daniel.prudhomme.pro@gmail.com>
2019-08-25 15:53:44 +0200

    Angular Scaffold components and services

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ScoreComponent } from './components/score/score.component';
+import { VoteComponent } from './components/vote/vote.component';
+
+const routes: Routes = [
+  { path: '', component: VoteComponent },
+  { path: 'score', component: ScoreComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VoteComponent } from './components/vote/vote.component';
+import { ScoreComponent } from './components/score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoteComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
